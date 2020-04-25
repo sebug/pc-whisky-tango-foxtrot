6b1d9877f68ec823211b200373acf5f7a56612fc
sebug <sebastian.gfeller@gmail.com>
2020-04-25 11:55:26 +0200

    Trying to define the game

---
+++ PCWhiskeyTangoFoxtrot/PCWhiskeyTangoFoxtrot/Models/Game.swift
@@ -0,0 +1,13 @@
+//
+//  Game.swift
+//  PCWhiskeyTangoFoxtrot
+//
+//  Created by Sebastian Gfeller on 25.04.20.
+//  Copyright © 2020 sebugtech. All rights reserved.
+//
+
+import Foundation
+
+struct Game {
+    let currentLetter: Letter
+}
