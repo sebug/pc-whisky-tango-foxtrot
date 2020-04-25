cc572f4baa8b6e68ef0b8f8b45a669c9c0b15b79
sebug <sebastian.gfeller@gmail.com>
2020-04-25 11:48:13 +0200

    Adding first struct

---
+++ PCWhiskeyTangoFoxtrot/PCWhiskeyTangoFoxtrot/Models/Letter.swift
@@ -0,0 +1,15 @@
+//
+//  Letter.swift
+//  PCWhiskeyTangoFoxtrot
+//
+//  Created by Sebastian Gfeller on 25.04.20.
+//  Copyright © 2020 sebugtech. All rights reserved.
+//
+
+import Foundation
+
+struct Letter {
+    let c : Character
+    let correctWord : String
+    let wrongOptions : Array<String>
+}
